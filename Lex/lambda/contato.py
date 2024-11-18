fd74c676abc031dbdfa50e087f9a29e610302b09
Nathalia de Oliveira Santos dos Reis <nathaliaosreis@gmail.com>
2024-11-17 23:32:33 -0300

    refactor: modificação da estrutura de resposta da intent contato

--- Lex/lambda/contato.py
+++ Lex/lambda/contato.py
@@ -4,8 +4,8 @@ def contato_intent(event):
     
     # Mensagens sobre os cuidados com a dengue
     message = "Caso você não tenha acesso ao foco de dengue ou ache que não é capaz de lidar com essa situação, entre em contato: "
-    message1 = "Telefone: xxxxxxxxx"
-    message2 = "Email: xxxx@gmail.com"
+    message1 = "Você pode ligar para os números: 199 ou 193"
+    message2 = "Para fazer reclamações: https://www.participa.df.gov.br/pages/registro-manifestacao/relato"
 
 
     # Estrutura de resposta para o Lex já com os aúdios pra cada mensagem
